c01a949f3669eb1e533880ded5af03f5b709b40f
양하늘 <skygksmf5810@gmail.com>
2026-06-16 18:43:06 +0900

    ♻️ Refactor: images.remotePatterns 확장

--- next.config.ts
+++ next.config.ts
@@ -19,6 +19,18 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: apiHostname,
       },
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+      },
+      {
+        protocol: 'http',
+        hostname: '127.0.0.1',
+      },
     ],
   },
   turbopack: {
