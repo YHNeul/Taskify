7a9f2dabc6cc0443e5c28d6000ca781b3c342eaf
양하늘 <skygksmf5810@gmail.com>
2026-07-13 21:21:02 +0900

    ♻️ Refactor: afterEach에 vi.clearAllMocks() 추가

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -3,6 +3,7 @@ import { afterEach, vi } from 'vitest';
 process.env.NEXT_PUBLIC_API_URL = 'https://example.com';
 
 afterEach(() => {
+  vi.clearAllMocks();
   vi.restoreAllMocks();
   vi.unstubAllGlobals();
 });
